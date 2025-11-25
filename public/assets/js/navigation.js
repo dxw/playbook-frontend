9d8266fea807555e0c9e450bc4380db6053bbced
Ynda Jas <yndajas@gmail.com>
2025-11-25 16:13:42 +0000

    Only scroll navigation on large viewports
    The small viewport navigation layout always has the current page in
    view, and scrolling the page brings you to a less sensible position in
    that context

--- public/assets/js/navigation.js
+++ public/assets/js/navigation.js
@@ -1,1 +1,4 @@
-document.querySelector(".current-page")?.scrollIntoView()
+// should be kept in sync with the large variable in the breakpoints stylesheet
+if (window.innerWidth >= 800) {
+  document.querySelector(".current-page")?.scrollIntoView()
+}
